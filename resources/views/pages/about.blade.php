1da75c901821e933087687b176c53f69802f72f9
silasmas <silasjmas@gmail.com>
2022-08-31 12:48:39 +0100

    ddf

--- resources/views/pages/about.blade.php
+++ resources/views/pages/about.blade.php
@@ -2,6 +2,26 @@
     <div id="about" class="rn-about-area rn-section-gap">
         <div class="container">
             <div class="row">
+                <div class="col-lg-3">
+                    <form action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_top">
+                        <input type="hidden" name="cmd" value="_s-xclick">
+                        <table>
+                        <tr><td><input type="hidden" name="on0" value="Quantity">Quantity</td></tr><tr><td><select name="os0">
+                            <option value="2">2 </option>
+                            <option value="5">5 </option>
+                            <option value="10">10 </option>
+                        </select> </td></tr>
+                        <tr><td><input type="hidden" name="on1" value="Votre nom">Votre nom</td></tr><tr><td><input type="text" name="os1" maxlength="200"></td></tr>
+                        <tr><td><input type="hidden" name="on2" value="Votre Email">Votre Email</td></tr><tr><td><input type="text" name="os2" maxlength="200"></td></tr>
+                        </table>
+                        <input type="hidden" name="encrypted" value="-----BEGIN PKCS7-----MIIHdwYJKoZIhvcNAQcEoIIHaDCCB2QCAQExggEwMIIBLAIBADCBlDCBjjELMAkGA1UEBhMCVVMxCzAJBgNVBAgTAkNBMRYwFAYDVQQHEw1Nb3VudGFpbiBWaWV3MRQwEgYDVQQKEwtQYXlQYWwgSW5jLjETMBEGA1UECxQKbGl2ZV9jZXJ0czERMA8GA1UEAxQIbGl2ZV9hcGkxHDAaBgkqhkiG9w0BCQEWDXJlQHBheXBhbC5jb20CAQAwDQYJKoZIhvcNAQEBBQAEgYAnq1KEt1u5HbofIUUs3KxkQ63fAyKSpHJohNhbJRQG8J76+GFqPbvC62fAbbTEuhZDbi4rDnJE+lGjoAcrf3svewT9+ujcRJ9dkppWq73c167qQx9yztEdQDGcUGQZC7hKHwVTSVNiv8c1wrn9Y07UmvJ6tEIh4pp5QOvwnFAFMDELMAkGBSsOAwIaBQAwgfQGCSqGSIb3DQEHATAUBggqhkiG9w0DBwQI+BSd4zg4ndSAgdBWwUUG81kjaCB4Lcw4oPgqyKNz7aRNLxtlUA7MXYt8jJ9YFciUwAElguSlL2v5ZLH6daiN8TUzHXaRG13tVbPwgaccgHuIFad7dm0E3vbS/E87s8NUTGh04ZWHULwGvi+gdYIfd+zj4d82G7JWC4zVSXROnhG/v21aZ/DHutTBDuPs4iydD+h+uwqlxM1fbZhTK/NV+G0GGkt+64Lzks5HhExTbYdalMmT7mVszHmhcq7g01jfTexi5f3zIITcCbsS160ZcOcSRlfP61Pt4bJroIIDhzCCA4MwggLsoAMCAQICAQAwDQYJKoZIhvcNAQEFBQAwgY4xCzAJBgNVBAYTAlVTMQswCQYDVQQIEwJDQTEWMBQGA1UEBxMNTW91bnRhaW4gVmlldzEUMBIGA1UEChMLUGF5UGFsIEluYy4xEzARBgNVBAsUCmxpdmVfY2VydHMxETAPBgNVBAMUCGxpdmVfYXBpMRwwGgYJKoZIhvcNAQkBFg1yZUBwYXlwYWwuY29tMB4XDTA0MDIxMzEwMTMxNVoXDTM1MDIxMzEwMTMxNVowgY4xCzAJBgNVBAYTAlVTMQswCQYDVQQIEwJDQTEWMBQGA1UEBxMNTW91bnRhaW4gVmlldzEUMBIGA1UEChMLUGF5UGFsIEluYy4xEzARBgNVBAsUCmxpdmVfY2VydHMxETAPBgNVBAMUCGxpdmVfYXBpMRwwGgYJKoZIhvcNAQkBFg1yZUBwYXlwYWwuY29tMIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQDBR07d/ETMS1ycjtkpkvjXZe9k+6CieLuLsPumsJ7QC1odNz3sJiCbs2wC0nLE0uLGaEtXynIgRqIddYCHx88pb5HTXv4SZeuv0Rqq4+axW9PLAAATU8w04qqjaSXgbGLP3NmohqM6bV9kZZwZLR/klDaQGo1u9uDb9lr4Yn+rBQIDAQABo4HuMIHrMB0GA1UdDgQWBBSWn3y7xm8XvVk/UtcKG+wQ1mSUazCBuwYDVR0jBIGzMIGwgBSWn3y7xm8XvVk/UtcKG+wQ1mSUa6GBlKSBkTCBjjELMAkGA1UEBhMCVVMxCzAJBgNVBAgTAkNBMRYwFAYDVQQHEw1Nb3VudGFpbiBWaWV3MRQwEgYDVQQKEwtQYXlQYWwgSW5jLjETMBEGA1UECxQKbGl2ZV9jZXJ0czERMA8GA1UEAxQIbGl2ZV9hcGkxHDAaBgkqhkiG9w0BCQEWDXJlQHBheXBhbC5jb22CAQAwDAYDVR0TBAUwAwEB/zANBgkqhkiG9w0BAQUFAAOBgQCBXzpWmoBa5e9fo6ujionW1hUhPkOBakTr3YCDjbYfvJEiv/2P+IobhOGJr85+XHhN0v4gUkEDI8r2/rNk1m0GA8HKddvTjyGw/XqXa+LSTlDYkqI8OwR8GEYj4efEtcRpRYBxV8KxAW93YDWzFGvruKnnLbDAF6VR5w/cCMn5hzGCAZowggGWAgEBMIGUMIGOMQswCQYDVQQGEwJVUzELMAkGA1UECBMCQ0ExFjAUBgNVBAcTDU1vdW50YWluIFZpZXcxFDASBgNVBAoTC1BheVBhbCBJbmMuMRMwEQYDVQQLFApsaXZlX2NlcnRzMREwDwYDVQQDFAhsaXZlX2FwaTEcMBoGCSqGSIb3DQEJARYNcmVAcGF5cGFsLmNvbQIBADAJBgUrDgMCGgUAoF0wGAYJKoZIhvcNAQkDMQsGCSqGSIb3DQEHATAcBgkqhkiG9w0BCQUxDxcNMjIwODMxMTEzMjAzWjAjBgkqhkiG9w0BCQQxFgQU6MnGe7aTfhkqfy6fuJiv6ZTM6PswDQYJKoZIhvcNAQEBBQAEgYCsT5h5yLuK+JyBySlYIOfK6LpTP+VuvoJehcWCYEtkGFiJy5WDOyV3gkVgHU5nDhRWo6cRxaC486fuFzdH769szJLAFWwhtdxmgW9cX8/x20+buu3sdLt8gkPmy8GwpcxT1zvwox5INCnYwKDB8EmCZEphZTXC9ViTgy8yWTQ78w==-----END PKCS7-----">
+                        <input type="image" src="https://www.paypalobjects.com/fr_XC/i/btn/btn_buynowCC_LG.gif" border="0" name="submit" alt="PayPal - la solution de paiement en ligne la plus simple et la plus sécurisée !">
+                        <img alt="" border="0" src="https://www.paypalobjects.com/fr_XC/i/scr/pixel.gif" width="1" height="1">
+                        </form>
+
+                </div>
+            </div>
+            <div class="row">
                 <div class="col-lg-5">
                     <div data-aos="fade-up" data-aos-duration="1000" data-aos-delay="100" data-aos-once="true" class="image-area">
                         <div class="thumbnail">
